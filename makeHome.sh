75d9da69e6914a1dcaa52cdc3ea4a38d7da59399
Tsing <tsing.goo@gmail.com>
2016-02-04 12:34:35 -0500

    Building new file structure

--- makeHome.sh
+++
@@ -1,1 +0,0 @@
-g++ -g -std=c++11 -o test *.cpp imgui/*.cpp -lGL -lGLU -lglfw -lX11 -lXxf86vm -lXrandr -lpthread -lXi -lGLEW -lassimp -lIL -lILU -lILUT -lpython2.7
--- makeMac.sh
+++
@@ -1,1 +0,0 @@
-g++ -Wall -std=c++11 -o test *.cpp imgui/*.cpp -I /usr/local/include -framework OpenGL -L /usr/local/lib -lglfw3 -lglew -lassimp -lil -lILU -lpython2.7
