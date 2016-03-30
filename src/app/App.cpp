827e5bcd57733a6919a74dc2d352b12b26f91813
Tsing <tsing.goo@gmail.com>
2016-03-30 02:24:32 -0400

    Apple compiling issue fixed

--- src/app/App.cpp
+++ src/app/App.cpp
@@ -26,12 +26,12 @@ bool App::startGL()
         return false;
     }
     // uncomment these lines if on Apple OS X
-//#ifdef _APPLE_
+#ifdef __APPLE__
     glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//#endif
+#endif
 
     const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
     windowWidth=mode->width;
